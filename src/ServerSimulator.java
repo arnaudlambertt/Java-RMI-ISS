1159369c0dfb7ea73f4d4b7c57c0811f67cbfa5d
thobias00 <thobias00@hotmail.com>
2022-09-13 15:49:35 +0200

    feat: ServerSimulator, Proxy

---
+++ src/ServerSimulator.java
@@ -0,0 +1,11 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerSimulator{
+    public ServerSimulator() {
+    }
+    public static void main(String[] args) {
+
+    }
+}
